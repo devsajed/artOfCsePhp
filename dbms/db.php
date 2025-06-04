82ed83d435d9352ec6f4f92323ed23e99e88b19d
unknown <sajedislam019@gmail.com>
2025-06-05 00:04:06 +0600

    file update

--- dbms/db.php
+++ dbms/db.php
@@ -1,11 +1,38 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-  <meta charset="UTF-8">
-  <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <title>Database Management Syestem</title>
-</head>
-<body>
-<?php echo "Database Management Syestem";?>
-</body>
-</html>
+<?php
+require_once "connect.php";
+
+// SELECT FROM DATA BASE 
+/*
+
+$sql = "SELECT * FROM users";
+$result = mysqli_query($conn, $sql);
+
+var_dump($result);
+
+while($row = mysqli_fetch_assoc($result)){
+  echo "ID : " . $row["id"]  . $row["name" ]  . $row["email" ] . "<br>" ;
+  echo "NAME : " . $row["name"] . "<br>";
+
+  var_dump($row);
+
+}
+*/
+
+// UPDATE FROM DATA BASE 
+/*
+$sql = "UPDATE users SET name = 'karim', age=20 ,email='karim@gmail.com', password='12345' WHERE id = 5";
+  if(mysqli_query($conn,$sql)){
+    echo "updated";
+  }else{
+    echo"not update";
+  };
+*/
+
+// DELETE FROM DATA BASE 
+/*
+$sql = "DELETE FROM  users WHERE id = 4";
+
+if(mysqli_query($conn,$sql)){
+  echo "Delete successfully";
+};
+*/
