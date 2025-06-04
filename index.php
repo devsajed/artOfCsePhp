db8922ead3cf688cc22081850ab28a56d43f49b9
unknown <sajedislam019@gmail.com>
2025-06-04 15:17:11 +0600

    Art of cse php course git added

---
+++ index.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>art of CSE course</title>
+</head>
+<body>
+  <?php 
+  echo "helo  world";
+  ?>
+</body>
+</html>
